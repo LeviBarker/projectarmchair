c83bc2c9d47363eb3c11e7a46530bd9a75c9b5b0
Levi Barker <levi.barker@outlook.com>
2024-12-15 09:20:56 -0600

    added faq

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
