6a6bc12954bf9412a90ae4fe431c1b63038b3942
Levi Barker <levi.barker@outlook.com>
2025-07-07 12:39:39 -0500

    homepage changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import {BookOpenIcon} from "@heroicons/react/24/outline";
-import {ChevronDownIcon} from "@heroicons/react/16/solid";
-import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/react";
 import Hero from "@/components/Hero";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { BookOpenIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,7 +11,7 @@ export default function Home() {
                 <div className="lg:mt-0 mt-32 lg:flex gap-3">
                     <div className="max-w-lg relative">
                         <BookOpenIcon
-                            className="animation-delay-600ms animate-fade-in-and-up absolute -top-6 -left-6 h-32 z-0 text-red-200"/>
+                            className="animation-delay-600ms animate-fade-in-and-up absolute -top-6 -left-6 h-32 z-0 text-red-200" />
                         <h1 className="animation-delay-200ms animate-fade-in-and-up text-5xl font-bold pb-4">Teachers
                             reading aloud to children in <em className="italic text-red-500">crisis</em>.</h1>
                         <p className="animation-delay-400ms animate-fade-in-and-up">
@@ -21,46 +21,48 @@ export default function Home() {
                             readings of the gifted book.
                         </p>
                     </div>
-                    <div className="relative" style={{zIndex: 9}}>
+                    <div className="relative" style={{ zIndex: 9 }}>
                         <video className="lg:mt-0 mt-8 w-[560px] rounded-xl shadow"
-                               poster="https://static.wixstatic.com/media/e4c3dd_8e7d6ce21335478e933e2f293da5e927~mv2.jpg/v1/fill/w_1960,h_912,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Young%2520Girl%2520Reading_edited.jpg"
-                               controls={true} crossOrigin="anonymous" playsInline={true}
-                               preload="auto" loop={true} tabIndex={-1} autoPlay={false}
-                               src="https://video.wixstatic.com/video/e4c3dd_d61698dff1a24cdeb588c0729cc0fab2/360p/mp4/file.mp4"></video>
+                            poster="https://static.wixstatic.com/media/e4c3dd_8e7d6ce21335478e933e2f293da5e927~mv2.jpg/v1/fill/w_1960,h_912,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Young%2520Girl%2520Reading_edited.jpg"
+                            controls={true} crossOrigin="anonymous" playsInline={true}
+                            preload="auto" loop={true} tabIndex={-1} autoPlay={false}
+                            src="https://video.wixstatic.com/video/e4c3dd_d61698dff1a24cdeb588c0729cc0fab2/360p/mp4/file.mp4"></video>
                     </div>
                 </div>
                 <BookOpenIcon
-                    className="md:inline hidden z-0 text-teal-300 animate-fade-in-and-up-right absolute bottom-8 right-8 h-12 rotate-6"/>
+                    className="md:inline hidden z-0 text-teal-300 animate-fade-in-and-up-right absolute bottom-8 right-8 h-12 rotate-6" />
                 <BookOpenIcon
-                    className="md:inline hidden z-0 text-red-400 animate-fade-in-and-up-left animation-delay-200ms absolute bottom-12 right-32 h-12 rotate-12"/>
+                    className="md:inline hidden z-0 text-red-400 animate-fade-in-and-up-left animation-delay-200ms absolute bottom-12 right-32 h-12 rotate-12" />
                 <BookOpenIcon
-                    className="md:inline hidden z-0 text-red-500 animate-fade-in-and-up-right animation-delay-400ms absolute bottom-10 right-56 h-10 -rotate-6"/>
+                    className="md:inline hidden z-0 text-red-500 animate-fade-in-and-up-right animation-delay-400ms absolute bottom-10 right-56 h-10 -rotate-6" />
                 <BookOpenIcon
-                    className="md:inline hidden z-0 text-teal-400 animate-fade-in-and-up-left animation-delay-600ms absolute bottom-24 right-44 h-12 rotate-12"/>
+                    className="md:inline hidden z-0 text-teal-400 animate-fade-in-and-up-left animation-delay-600ms absolute bottom-24 right-44 h-12 rotate-12" />
                 <BookOpenIcon
-                    className="md:inline hidden z-0 text-red-300 animate-fade-in-and-up-right animation-delay-800ms absolute bottom-28 right-16 h-16 -rotate-12"/>
+                    className="md:inline hidden z-0 text-red-300 animate-fade-in-and-up-right animation-delay-800ms absolute bottom-28 right-16 h-16 -rotate-12" />
             </Hero>
             <div className="flex items-center justify-start p-8 bg-red-600">
                 <div className="w-full">
                     <h2 className="font-bold text-4xl pb-4 text-white w-full text-center underline">FAQ</h2>
                     <div className="w-full" data-cy="FAQs">
                         <Disclosure as="div"
-                                    className="p-4 bg-gray-100 text-stone-800 rounded shadow block mb-4">
+                            className="p-4 bg-gray-100 text-stone-800 rounded shadow block mb-4">
                             <DisclosureButton data-cy="Background"
-                                              className="py-2 group flex justify-between items-center w-full font-bold">
+                                className="py-2 group flex justify-between items-center w-full font-bold">
                                 Background
-                                <ChevronDownIcon className="h-5 group-data-[open]:rotate-180"/>
+                                <ChevronDownIcon className="h-5 group-data-[open]:rotate-180" />
 
                             </DisclosureButton>
                             <DisclosurePanel className="text-stone-600 min-h-64">
                                 <div className="flex gap-4 items-start justify-between pt-4">
-                                    <Image
-                                        src="https://firebasestorage.googleapis.com/v0/b/projectarmchair-37a48.firebasestorage.app/o/Vonda%20headshot.jpg?alt=media"
-                                        alt="Original logo"
-                                        className="rounded-full"
-                                        width={64}
-                                        height={32}
-                                    />
+                                    <div style={{ width: 400 }} >
+                                        <Image
+                                            src="https://firebasestorage.googleapis.com/v0/b/projectarmchair-37a48.firebasestorage.app/o/Vonda%20headshot.jpg?alt=media"
+                                            alt="Original logo"
+                                            className="rounded-full"
+                                            width={64}
+                                            height={32}
+                                        />
+                                    </div>
                                     <div>
                                         <p className="pb-4">
                                             My name is Vonda Dahl, and I am an educator with a background in elementary
@@ -77,7 +79,7 @@ export default function Home() {
                                             several of our students were living there.
                                         </p>
                                         <p className="pb-4">
-                                            I could not stop thinking about that or about what I might do to help. I
+                                            I could not stop thinking about that or about what I might do to help. It
                                             eventually
                                             dawned on me that as a reading specialist, I could read to kids, possibly
                                             help them
@@ -107,11 +109,11 @@ export default function Home() {
                             </DisclosurePanel>
                         </Disclosure>
                         <Disclosure as="div"
-                                    className="p-4 bg-gray-100 text-stone-800 rounded shadow block mb-4">
+                            className="p-4 bg-gray-100 text-stone-800 rounded shadow block mb-4">
                             <DisclosureButton data-cy="Mission"
-                                              className="py-2 group flex justify-between items-center w-full font-bold">
+                                className="py-2 group flex justify-between items-center w-full font-bold">
                                 Mission
-                                <ChevronDownIcon className="h-5 group-data-[open]:rotate-180"/>
+                                <ChevronDownIcon className="h-5 group-data-[open]:rotate-180" />
 
                             </DisclosureButton>
                             <DisclosurePanel className="text-stone-600">
@@ -126,11 +128,11 @@ export default function Home() {
                             </DisclosurePanel>
                         </Disclosure>
                         <Disclosure as="div"
-                                    className="p-4 bg-gray-100 text-stone-800 rounded shadow block mb-4">
+                            className="p-4 bg-gray-100 text-stone-800 rounded shadow block mb-4">
                             <DisclosureButton data-cy="vision"
-                                              className="py-2 group flex justify-between items-center w-full font-bold">
+                                className="py-2 group flex justify-between items-center w-full font-bold">
                                 Vision
-                                <ChevronDownIcon className="h-5 group-data-[open]:rotate-180"/>
+                                <ChevronDownIcon className="h-5 group-data-[open]:rotate-180" />
 
                             </DisclosureButton>
                             <DisclosurePanel className="text-stone-600">
@@ -142,98 +144,98 @@ export default function Home() {
                                     <span className="font-bold">
                                         The Sun…
                                     </span>
-                                    <br/>
+                                    <br />
                                     streams through tall windows
-                                    <br/>
+                                    <br />
                                     in yellow shafts of warmth,
-                                    <br/>
+                                    <br />
                                     The room is quiet,
-                                    <br/>
+                                    <br />
                                     save for the gentle tick of the mantle clock.
-                                    <br/>
+                                    <br />
                                     And speckles of golden dust
-                                    <br/>
+                                    <br />
                                     float noiselessly in the vacuum of sound.
-                                    <br/>
+                                    <br />
                                     The Chair…
-                                    <br/>
+                                    <br />
                                     is massive, soft, and well-worn.
-                                    <br/>
-                                    like the hug of an old, fuzzy, stuffed bear.
-                                    <br/>
+                                    <br />
+                                    like the hug of an old, stuffed bear.
+                                    <br />
                                     A child is there,
-                                    <br/>
-                                    lost in hug of the chair,
-                                    <br/>
+                                    <br />
+                                    lost in the hug of the chair,
+                                    <br />
                                     mesmerized by hush of the sanctuary.
-                                    <br/>
+                                    <br />
                                     The book in her lap is a portal
-                                    <br/>
+                                    <br />
                                     to another universe.
-                                    <br/>
+                                    <br />
                                     One without pain, fear, hunger, or uncertainty.
-                                    <br/>
+                                    <br />
                                     The child in the enormous chair
-                                    <br/>
+                                    <br />
                                     does not hear the tick of the clock,
-                                    <br/>
+                                    <br />
                                     or see the golden specks
-                                    <br/>
+                                    <br />
                                     that float around her head.
-                                    <br/>
+                                    <br />
                                     She is only cognizant
-                                    <br/>
+                                    <br />
                                     of the place she has entered
-                                    <br/>
+                                    <br />
                                     through the Portal.
-                                    <br/>
+                                    <br />
                                     Her heart is light
-                                    <br/>
+                                    <br />
                                     and her world at peace.
-                                    <br/>
+                                    <br />
                                     For this magic, sacred moment,
-                                    <br/>
+                                    <br />
                                     all is well.
                                 </p>
                                 <p className="italic">
                                     <span className="font-bold">The Chair...</span>
-                                    <br/>
+                                    <br />
                                     is massive, soft, and well-worn.
-                                    <br/>
+                                    <br />
                                     like the hug of an old, fuzzy, stuffed bear.
-                                    <br/>
+                                    <br />
                                     A child is there,
-                                    <br/>
+                                    <br />
                                     lost in hug of the chair,
-                                    <br/>
+                                    <br />
                                     mesmerized by hush of the sanctuary.
-                                    <br/>
+                                    <br />
                                     The book in her lap is a portal
-                                    <br/>
+                                    <br />
                                     to another universe.
-                                    <br/>
+                                    <br />
                                     One without pain, fear, hunger, or uncertainty.
-                                    <br/>
+                                    <br />
                                     The child in the enormous chair
-                                    <br/>
+                                    <br />
                                     does not hear the tick of the clock,
-                                    <br/>
+                                    <br />
                                     or see the golden specks
-                                    <br/>
+                                    <br />
                                     that float around her head.
-                                    <br/>
+                                    <br />
                                     She is only cognizant
-                                    <br/>
+                                    <br />
                                     of the place she has entered
-                                    <br/>
+                                    <br />
                                     through the Portal.
-                                    <br/>
+                                    <br />
                                     Her heart is light
-                                    <br/>
+                                    <br />
                                     and her world at peace.
-                                    <br/>
+                                    <br />
                                     For this magic, sacred moment,
-                                    <br/>
+                                    <br />
                                     all is well.
                                 </p>
                             </DisclosurePanel>
